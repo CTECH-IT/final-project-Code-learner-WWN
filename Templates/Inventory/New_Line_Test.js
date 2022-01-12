f66baf90a9994afff24fbdfe6d9ee47d92e1c1ff
William Nigbur <7578002@isd535.org>
2022-01-12 09:15:01 -0600

    finished the new line function

--- Templates/Inventory/New_Line_Test.js
+++ Templates/Inventory/New_Line_Test.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 //append content at the end of the file:
-fs.appendFile('Inventory.txt', 'Test', function (err) {
+fs.appendFile('Inventory.txt', '\nTest', function (err) {
   if (err) throw err;
   console.log('Updated!');
 });
