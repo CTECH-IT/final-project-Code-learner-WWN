2f7bcd0aecfb2c217b06ba2e71c483fff9206911
William Nigbur <7578002@isd535.org>
2022-01-14 09:42:15 -0600

    Stat uncomplete

---
+++ Templates/Statistics_Tests/Stat_Roll_Mk2.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var dt = require('./Stat_Roll.js');
+
+exports.Stat = function repeat(func, times) {
+    for (x = 0; x < times; x++); 
+}
--- Templates/Statistics_Tests/Stat_Set-up.js
+++ Templates/Statistics_Tests/Stat_Set-up.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
-var dt = require('./Stat_Roll.js');
+var dt = require('./Stat_Roll_MK2.js');
 
 //append content at the end of the file:
-fs.appendFile('Stats.txt', 'Your Strenght Stat is:\n' + dt.Rng(), function (err) {
+fs.appendFile('Stats.txt', 'Your Strenght Stat is:\n' + dt.Stat(), function (err) {
   if (err) throw err;
   console.log('Updated!');
 });
