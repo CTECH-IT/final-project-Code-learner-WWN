792991591a72dd12079be07c99a98fac98b3bd4f
William Nigbur <7578002@isd535.org>
2022-01-26 18:20:50 -0600

    There is an error with Stat_STR.js in Stat_Rework.

--- Templates/Statistics_Tests/Stat_Rework/Stat_STR.js
+++ Templates/Statistics_Tests/Stat_Rework/Stat_STR.js
@@ -4,5 +4,8 @@ var dt = require('./Stat_Roll_Com.js');
 
 //append content at the end of the file:
 exports.Data_STR = function(){
-    'Your Strenght Stat is: \n' + dt.Stat();
+    fs.appendFile('Stats.txt','Your Strenght Stat is: \n' + dt.Stat(), function (err){
+        if (err) throw err;
+        console.log('Created STR!');
+    });
 }
--- Templates/Statistics_Tests/Stat_Rework/Stat_Start.js
+++ Templates/Statistics_Tests/Stat_Rework/Stat_Start.js
@@ -1,8 +1,9 @@
 //Display one
 var fs = require('fs');
-var dt = require('./Stat_STR.js');
 
-    fs.appendFile('Stats.txt','The Stats for "Test_User" are: \n' +  dt.Data_STR() , function (err){
+exports.Data_STA = function(){    
+    fs.appendFile('Stats.txt','The Stats for "Test_User" are: \n', function (err){
         if (err) throw err;
         console.log('Created Stats!');
     });
+}
